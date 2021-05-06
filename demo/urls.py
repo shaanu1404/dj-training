d0108b2aa7f39d54d5259ec87f7a2dd19e2877ae
harish chandra tanty <shanu14tnt@gmail.com>
2021-05-06 14:56:20 +0530

    products CRUD

--- demo/urls.py
+++ demo/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 
-from products.views import all_products_view, single_product_view
+from products.views import all_products_view, single_product_view, create_new_product, edit_product, delete_product
 
 urlpatterns = [
     path('', all_products_view),    # localhost:8000/
-    path('<int:id>/', single_product_view), # localhost:8000/1
+    path('create/', create_new_product),  # localhost:8000/create
+    path('<int:id>/', single_product_view),  # localhost:8000/1
+    path('<int:id>/edit/', edit_product),  # localhost:8000/1/edit
+    path('<int:id>/delete/', delete_product),  # localhost:8000/1/delete
     path('admin/', admin.site.urls),
 ]
+
+
+# localhost:8000/  -- READ -- GET
+# localhost:8000/create  -- CREATE -- POST
+# localhost:8000/ID  -- READ -- GET
+# localhost:8000/ID/edit  -- EDIT -- PUT/PATCH
+# localhost:8000/ID/delete  -- DELETE -- DELETE
--- products/views.py
+++ products/views.py
@@ -1,20 +1,93 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Category
 
 
 def all_products_view(request):
-    all_products = Product.objects.all()
-    return render(request, 'products/list.html', {"all_products": all_products})
+    """Get all products"""
+
+    all_products = Product.objects.order_by('-id')
+    context = {"all_products": all_products}
+    return render(request, 'products/list.html', context)
 
 
 def single_product_view(request, id):
-    product = None
-    try:
-        product = Product.objects.get(id=id)
-    except Product.DoesNotExist:
-        print("Product does not exist")
+    """Get single product using id"""
+
+    # product = None
+    # try:
+    #     product = Product.objects.get(id=id)
+    # except Product.DoesNotExist:
+    #     print("Product does not exist")
+
+    product = get_object_or_404(Product, id=id)
 
     context = {
         "product": product
     }
     return render(request, 'products/detail.html', context)
+
+
+def create_new_product(request):
+    """Create new product"""
+
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        price = request.POST['price']
+        category_id = request.POST['category']
+
+        if title and description and price and category_id:
+            category = Category.objects.get(id=category_id)
+
+            new_product = Product(
+                title=title, description=description, price=price, category=category)
+            new_product.save()
+            return redirect('/')
+
+    context = {
+        "categories": Category.objects.all()
+    }
+    return render(request, 'products/create.html', context)
+
+
+def edit_product(request, id):
+    """Edit product using id"""
+
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        price = request.POST['price']
+        category_id = request.POST['category']
+
+        if title and description and price and category_id:
+            category = Category.objects.get(id=category_id)
+
+            product.title = title
+            product.description = description
+            product.price = price
+            product.category = category
+            product.save()
+            return redirect('/')
+
+    context = {
+        "product": product,
+        "categories": Category.objects.all()
+    }
+    return render(request, 'products/edit.html', context)
+
+
+def delete_product(request, id):
+    """Delete product using id"""
+
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('/')
+
+    context = {
+        "product": product
+    }
+    return render(request, 'products/delete.html', context)
